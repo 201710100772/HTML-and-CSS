a9288fd72ab5bb3a163c51a96850b5d2a314b426
201710100772 <60249338+201710100772@users.noreply.github.com>
2020-01-24 13:10:00 +0530

    Add files via upload

---
+++ p1.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+void main()
+{
+int d,fc;
+char tv;
+printf("enter the distance travelled\n");
+scanf("%d",&d);
+printf("enter the type of vehicle\n");
+scanf(" %c",&tv);
+
+if(tv== 'M')
+ {
+   fc=d*15;
+   printf("final cost=%d",fc);
+ }
+else if(tv=='I')
+ {
+   fc=d*20;
+   printf("final cost=%d",fc);
+ }
+else if(tv=='S')
+ {
+    fc=d*25;
+    printf("final cost=%d",fc);
+ }
+ 
+}
+ 
